10fc06cdd5d1623441c9b0bd05df1170c6e547e4
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-16 03:27:30 +0100

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import base64
+from st_aggrid import AgGrid
+
 
 # Adding Nav Bar
 st.markdown('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">',
@@ -27,6 +30,380 @@ st.markdown("""
 </nav>
 """, unsafe_allow_html=True)
 
+# App Header
 st.markdown('''# **Data Cleaning Web App**
 A simple Data Cleaning Web Application.
 ''')
+
+file_ = open("image.png", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="dashboard gif">',
+     unsafe_allow_html=True
+)
+
+# Styling side bar with image
+st.sidebar.image("gif.gif", use_column_width=True)
+
+
+# Multipage checkbox
+page = st.sidebar.selectbox('Select Page', ['Choose','Check Missing Value', 'Replace With Mean'
+                                            ,'Replace With Average', 'Replace With Mode', 'Replace With Standard Deviation',
+                                            'Replace With Minimum Value', 'Replace With Maximum Value'])
+
+# Check missing value
+if page == 'Check Missing Value':
+
+    df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+    try:
+        df_file = pd.read_csv(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+        # Open Excel File
+    try:
+        df_file = pd.read_excel(df_file, engine='openpyxl')
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+        # Read Pickle File
+    try:
+        df_file = pd.read_pickle(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+    try:
+        df = df_file.isnull().sum()
+        if st.button('view Data'):
+            st.write(df)
+
+    except:
+        pass
+
+# Replace Null Value Mean
+if page == 'Replace With Mean':
+
+    df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+    try:
+        df_file = pd.read_csv(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+        # Open Excel File
+    try:
+        df_file = pd.read_excel(df_file, engine='openpyxl')
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+        # Read Pickle File
+    try:
+        df_file = pd.read_pickle(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+    try:
+        df = df_file.fillna(df_file.mean().round(0))
+        if st.button('Clean Data'):
+            st.write(df)
+
+        df1 = df.isnull().sum()
+        if st.button('View Null Value'):
+            st.write(df1)
+
+            df = pd.DataFrame(df)
+            file_name = "clean_data.csv"
+            file_path = f"./{file_name}"
+
+            df.to_csv(file_path)
+
+            df = open(file_path, 'rb')
+            st.download_button(label='Click to download',
+                               data=df,
+                               file_name=file_name,
+                               key='download_df')
+            df.close()
+
+
+    except:
+        pass
+
+# Replace with Average
+if page == 'Replace With Average':
+
+    df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+    try:
+        df_file = pd.read_csv(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+        # Open Excel File
+    try:
+        df_file = pd.read_excel(df_file, engine='openpyxl')
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+        # Read Pickle File
+    try:
+        df_file = pd.read_pickle(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+    try:
+        df = df_file.fillna(df_file.median().round(0))
+        if st.button('Clean Data'):
+            st.write(df)
+
+        df1 = df.isnull().sum()
+        if st.button('View Null Value'):
+            st.write(df1)
+
+            df = pd.DataFrame(df)
+            file_name = "clean_data.csv"
+            file_path = f"./{file_name}"
+
+            df.to_csv(file_path)
+
+            df = open(file_path, 'rb')
+            st.download_button(label='Click to download',
+                               data=df,
+                               file_name=file_name,
+                               key='download_df')
+            df.close()
+
+
+    except:
+        pass
+
+# Replace With Mode
+if page == 'Replace With Mode':
+
+    df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+    try:
+        df_file = pd.read_csv(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+        # Open Excel File
+    try:
+        df_file = pd.read_excel(df_file, engine='openpyxl')
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+        # Read Pickle File
+    try:
+        df_file = pd.read_pickle(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+    try:
+        df1 = df_file.fillna(df_file.mode().round(2))
+        if st.button('Clean Data'):
+            st.write(df1)
+
+        df2 = df1.isnull().sum()
+        if st.button('View Null Value'):
+            st.write(df2)
+
+            df1 = pd.DataFrame(df1)
+            file_name = "clean_data.csv"
+            file_path = f"./{file_name}"
+
+            df1.to_csv(file_path)
+
+            df1 = open(file_path, 'rb')
+            st.download_button(label='Click to download',
+                               data=df1,
+                               file_name=file_name,
+                               key='download_df')
+            df1.close()
+
+
+    except:
+        pass
+
+# Replace With Standard Deviation
+if page == 'Replace With Standard Deviation':
+
+    df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+    try:
+        df_file = pd.read_csv(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+        # Open Excel File
+    try:
+        df_file = pd.read_excel(df_file, engine='openpyxl')
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+        # Read Pickle File
+    try:
+        df_file = pd.read_pickle(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+    try:
+        df = df_file.fillna(df_file.std().round(0))
+        if st.button('Clean Data'):
+            st.write(df)
+
+        df1 = df.isnull().sum()
+        if st.button('View Null Value'):
+            st.write(df1)
+
+            df = pd.DataFrame(df)
+            file_name = "clean_data.csv"
+            file_path = f"./{file_name}"
+
+            df.to_csv(file_path)
+
+            df = open(file_path, 'rb')
+            st.download_button(label='Click to download',
+                               data=df,
+                               file_name=file_name,
+                               key='download_df')
+            df.close()
+
+
+    except:
+        pass
+
+# Replace with Minimum Value
+if page == 'Replace With Minimum Value':
+
+    df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+    try:
+        df_file = pd.read_csv(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+        # Open Excel File
+    try:
+        df_file = pd.read_excel(df_file, engine='openpyxl')
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+        # Read Pickle File
+    try:
+        df_file = pd.read_pickle(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+
+
+        df2 = df_file.fillna(df_file.min().round(0))
+        if st.button('Clean Data'):
+            st.write(df2)
+
+        df3 = df2.isnull().sum()
+        if st.button('View Null Value'):
+            st.write(df3)
+
+            df2 = pd.DataFrame(df2)
+            file_name = "clean_data.csv"
+            file_path = f"./{file_name}"
+
+            df2.to_csv(file_path)
+
+            df2 = open(file_path, 'rb')
+            st.download_button(label='Click to download',
+                               data=df2,
+                               file_name=file_name,
+                               key='download_df')
+            df2.close()
+
+
+    except:
+        pass
+
+# Replace with Maximum Value
+if page == 'Replace With Maximum Value':
+
+    df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+    try:
+        df_file = pd.read_csv(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+        # Open Excel File
+    try:
+        df_file = pd.read_excel(df_file, engine='openpyxl')
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+        # Read Pickle File
+    try:
+        df_file = pd.read_pickle(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+    try:
+        df = df_file.fillna(df_file.max().round(0))
+        if st.button('Clean Data'):
+            st.write(df)
+
+        df1 = df.isnull().sum()
+        if st.button('View Null Value'):
+            st.write(df1)
+
+            df = pd.DataFrame(df)
+            file_name = "clean_data.csv"
+            file_path = f"./{file_name}"
+
+            df.to_csv(file_path)
+
+            df = open(file_path, 'rb')
+            st.download_button(label='Click to download',
+                               data=df,
+                               file_name=file_name,
+                               key='download_df')
+            df.close()
+
+
+    except:
+        pass
+
+
+
