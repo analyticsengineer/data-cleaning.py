a8d53e654a94b1a8f3b757a083fd7849826828df
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-16 03:18:33 +0100

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -11,16 +11,16 @@ st.markdown("""
  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
  </button>
- <div class="collapse navbar-collapse" id="navbarNav">
-   <ul class="navbar-nav">
-    <li> class="nav-item active">
-     <a class="nav-link disabled" href="#">Home<span class="sr-only">(curent)</span></a>
-     </li>
-     <li class="nav-item">
-      <a class="nav-link" href="https://github.com/Designegycreatives/datacleaning-app.py" target="_blank">GitHub</a>
-     </li>
-     <li class="nav-item">
-      <a class="nav-link" href="https://www.linkedin.com/in/anuoluwapo-abiodun-balogun-64b977186/" target="_blank">LinkedIn</a>
+   <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li> class="nav-item active">
+           <a class="nav-link disabled" href="#">Home<span class="sr-only">(curent)</span></a>
+        </li>
+        <li class="nav-item">
+           <a class="nav-link" href="https://github.com/Designegycreatives/datacleaning-app.py" target="_blank">GitHub</a>
+        </li>
+        <li class="nav-item">
+           <a class="nav-link" href="https://www.linkedin.com/in/anuoluwapo-abiodun-balogun-64b977186/" target="_blank">LinkedIn</a>
         </li>
       </ul>
    </div>
