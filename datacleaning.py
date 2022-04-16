ed2e64a332e66bc3af1298604e90ed5b766579fb
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-16 03:08:58 +0100

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Adding Nav Bar
-st.markdown('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">',
+st.markdown('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">',
             unsafe_allow_html=True)
 
 st.markdown("""
