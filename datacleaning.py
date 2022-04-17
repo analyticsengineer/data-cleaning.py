1c952c8d330fabe63dd8fc8bfef7d8185f0aa374
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-17 02:03:19 +0100

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -51,7 +51,7 @@ st.sidebar.image("gif.gif", use_column_width=True)
 
 
 # Multipage checkbox
-page = st.sidebar.selectbox('Select Page', ['Choose','Check Missing Value', 'Replace With Mean'
+page = st.sidebar.selectbox('Select Page', ['Choose','Check Missing Value', 'Check Duplicated Value', 'Replace With Mean'
                                             ,'Replace With Average', 'Replace With Mode', 'Replace With Standard Deviation'])
 
 # Check missing value
@@ -85,6 +85,64 @@ if page == 'Check Missing Value':
         df = df_file.isnull().sum()
         if st.button('View Missing Values'):
             st.write(df)
+
+    except:
+        pass
+
+if page == 'Check Duplicated Value':
+            df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+    try:
+        df_file = pd.read_csv(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+        # Open Excel File
+    try:
+        df_file = pd.read_excel(df_file, engine='openpyxl')
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+        # Read Pickle File
+    try:
+        df_file = pd.read_pickle(df_file)
+        st.markdown("Your Data Record: ")
+        AgGrid(df_file, editable=True)
+    except:
+        pass
+
+    try:
+        df = df_file.duplicated()
+        if st.button('View Duplicated Values'):
+            st.write(df)
+
+    except:
+        pass
+    try:
+        df = df_file.drop_duplicates(include='object', keep=False, inplace=True)
+        if st.button('Clean Data'):
+            st.write(df)
+
+        df1 = df.isnull().sum()
+        if st.button('View Null Value'):
+            st.write(df1)
+
+            df = pd.DataFrame(df)
+            file_name = "clean_data.csv"
+            file_path = f"./{file_name}"
+
+            df.to_csv(file_path)
+
+            df = open(file_path, 'rb')
+            st.download_button(label='Click to download',
+                               data=df,
+                               file_name=file_name,
+                               key='download_df')
+            df.close()
+
 
     except:
         pass
