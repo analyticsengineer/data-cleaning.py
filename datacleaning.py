fe00eea89b4d1c9e143a025ee12e829b6a265362
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-17 02:25:46 +0100

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -116,7 +116,7 @@ if page == 'Check Duplicated Value':
         pass
 
     try:
-        df = df_file.groupby(df_file.columns.tolist('object'), as_index=False).size()
+        df = df_file.duplicated()
         if st.button('View Duplicated Values'):
             st.write(df)
 
