0d65f0fed37ed1200cce2f9b66057dda6e152a7d
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-23 02:31:20 +0100

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -390,7 +390,7 @@ if page == 'Splitting Column':
         
        col_df = df_file[df_file[col_clean].isin(selected_options)]
        
-       df_clean1 = df_file(col_df).str.split(',', expand=True)
+       df_clean1 = df_file[col_df].str.split(',', expand=True)
       
        if st.button('split by comma'):
               st.write(df_clean1)
