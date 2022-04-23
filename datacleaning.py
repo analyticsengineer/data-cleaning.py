2b102ca8da9b9a3aacb760a21241094f510317ba
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-23 20:51:09 +0100

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -527,8 +527,8 @@ if page == 'Fill Date Time':
         pass
 
     try:
-                     
-        df_date = df_file.fillna(0)
+        time = df_file.select_dtypes(include=['datetime','timestamps'])             
+        df_date = time.fillna(0)
         if st.button('Clean Data'):
             st.write(df_date)
 
