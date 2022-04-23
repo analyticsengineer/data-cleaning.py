c12b1790ff6cd80a294882619845c72fd7d7ade5
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-23 02:30:03 +0100

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -386,9 +386,11 @@ if page == 'Splitting Column':
 
     try:
        
-       col_clean = st.multiselect("Choose Column:",df_file.columns)                 
+       col_clean = st.multiselect("Choose Column:",df_file.columns)
+        
+       col_df = df_file[df_file[col_clean].isin(selected_options)]
        
-       df_clean1 = df_file[df_file[col_clean]].str.split(',', expand=True)
+       df_clean1 = df_file(col_df).str.split(',', expand=True)
       
        if st.button('split by comma'):
               st.write(df_clean1)
