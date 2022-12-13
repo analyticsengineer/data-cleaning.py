46ac06a809d22f9a19d18d123552dfe11b4ff851
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-12-13 14:20:15 +0100

    Create Check_the_number_of_rows_in_your_data.py

---
+++ ~/app/pages/Check_the_number_of_rows_in_your_data.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+from openpyxl import load_workbook
+import datetime
+import time
+from PIL import Image 
+
+col1, col2 = st.columns(2)
+image = Image.open('gif.gif')
+
+col1.header("Check For Missing Values In Your Data")
+col1.write("Misisng Values Creates Outliers In Your Data")
+col2.image(image)
+
+
+
+df_file = st.file_uploader("Upload your file: ", type=['csv', 'xlsx', 'pickle'])
+try:
+  df_file = pd.read_csv(df_file)
+  st.markdown("Your Data Record: ")
+  st.dataframe(df_file)
+except:
+  st.write("Upload A CSV, EXCEL OR PICKLE FILE")
+
+# Open Excel File
+try:
+  df_file = pd.read_excel(df_file, engine='openpyxl')
+  st.markdown("Your Data Record: ")
+  st.dataframe(df_file)
+except:
+  pass
+
+# Read Pickle File
+try:
+  df_file = pd.read_pickle(df_file)
+  st.markdown("Your Data Record: ")
+  st.dataframe(df_file)
+except:
+  pass
+
+try:
+  df = len(df_file)
+  st.write("The number of rows in your data is ".format(df))
+
+except:
+  pass
