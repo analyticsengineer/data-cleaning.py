2b77ee31d00a26189563b147a11b624609e75aec
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2023-06-20 18:03:44 +0100

    Update Replace_Object_Null_Values_With_Mode.py

--- ~/app/pages/Replace_Object_Null_Values_With_Mode.py
+++ ~/app/pages/Replace_Object_Null_Values_With_Mode.py
@@ -49,32 +49,31 @@ except:
 try:
    col_mode = st.multiselect("Choose Column:",options=df_file.columns)
    for column in col_mode:
-   col_mode.fillna(col_mode.mode()[0], inplace=True)
-   if st.button('Clean Data'):
-     st.write(df_file)
+     col_mode.fillna(col_mode.mode()[0], inplace=True)
+     if st.button('Clean Data'):
+       st.write(df_file)
+       
+       #for column in df.columns:
+       #df[column].fillna(df[column].mode()[0], inplace=True)
+  
+       if st.button('Clean Data'):
+       st.write(df)
 
-    
-   
-    #for column in df.columns:
-    #df[column].fillna(df[column].mode()[0], inplace=True)
-    if st.button('Clean Data'):
-     st.write(df)
+       df1 = df.isnull().sum()
+       if st.button('View Null Value'):
+         st.write(df1)
 
-   df1 = df.isnull().sum()
-   if st.button('View Null Value'):
-     st.write(df1)
+       df = pd.DataFrame(df)
+       file_name = "clean_data.csv"
+       file_path = f"./{file_name}"
 
-     df = pd.DataFrame(df)
-     file_name = "clean_data.csv"
-     file_path = f"./{file_name}"
-
-     df.to_csv(file_path)
-
-     df = open(file_path, 'rb')
-     st.download_button(label='Click to download',
-                      data=df,
-                      file_name=file_name,
-                      key='download_df')
-     df.close()
+       df.to_csv(file_path)
+  
+       df = open(file_path, 'rb')
+       st.download_button(label='Click to download',
+                        data=df,
+                        file_name=file_name,
+                        key='download_df')
+       df.close()
 except:
   pass
